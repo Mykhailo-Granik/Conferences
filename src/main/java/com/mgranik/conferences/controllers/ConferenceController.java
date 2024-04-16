6cb2c57539704926ef58df52a0a174d8c5d6b5e5
Mykhailo Granik <mgranik@axcient.com>
2024-04-16 20:18:08 +0300

    Sending error 400 on field validation

--- src/main/java/com/mgranik/conferences/controllers/ConferenceController.java
+++ src/main/java/com/mgranik/conferences/controllers/ConferenceController.java
@@ -3,6 +3,7 @@ package com.mgranik.conferences.controllers;
 import com.mgranik.conferences.dto.ConferenceDTO;
 import com.mgranik.conferences.entity.Conference;
 import com.mgranik.conferences.service.ConferenceService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,7 @@ public class ConferenceController {
     private final ConferenceService conferenceService;
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public Conference createConference(@RequestBody ConferenceDTO conference) {
+    public Conference createConference(@RequestBody @Valid ConferenceDTO conference) {
         return conferenceService.createConference(conference);
     }
 
--- src/main/java/com/mgranik/conferences/dto/ConferenceDTO.java
+++ src/main/java/com/mgranik/conferences/dto/ConferenceDTO.java
@@ -1,7 +1,16 @@
 package com.mgranik.conferences.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.ZonedDateTime;
 
-public record ConferenceDTO(String name, String topic, ZonedDateTime start, ZonedDateTime end, int participantsCount) {
+public record ConferenceDTO(
+        @NotBlank String name,
+        @NotBlank String topic,
+        @NotNull ZonedDateTime start,
+        @NotNull ZonedDateTime end,
+        @Min(101) int participantsCount) {
 
 }
