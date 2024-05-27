17003433a9eb54f5931e036623c88c6645ad51ff
Mykhailo Granik <mgranik@axcient.com>
2024-05-27 15:02:06 +0300

    Added tests for getting list of conferences and updating the conference

--- src/test/java/com/mgranik/conferences/controller/ConferenceControllerTest.java
+++ src/test/java/com/mgranik/conferences/controller/ConferenceControllerTest.java
@@ -12,11 +12,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 
 import static com.mgranik.conferences.tools.ZonedDateTimeMatcher.matchesZonedDateTime;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -34,20 +35,13 @@ class ConferenceControllerTest {
     @Test
     public void shouldCreateConference() throws Exception {
 
-        ConferenceDTO conferenceDTO = new ConferenceDTO(
-                "Test Conference",
-                "java",
-                ZonedDateTime.now(),
-                ZonedDateTime.now().plusDays(1),
-                101
-        );
-        Conference conference = Conference.fromDTO(conferenceDTO);
+        Conference conference = Conference.fromDTO(conferenceDTO());
 
         given(conferenceService.createConference(any())).willReturn(conference);
 
         mockMvc.perform(post("/conferences")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(conferenceDTO)))
+                        .content(objectMapper.writeValueAsString(conferenceDTO())))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name").value(conference.getName()))
                 .andExpect(jsonPath("$.start", matchesZonedDateTime(conference.getStart())))
@@ -57,5 +51,43 @@ class ConferenceControllerTest {
 
     }
 
+    private ConferenceDTO conferenceDTO() {
+        return new ConferenceDTO(
+                "Test Conference",
+                "java",
+                ZonedDateTime.now(),
+                ZonedDateTime.now().plusDays(1),
+                101
+        );
+    }
+
+    @Test
+    public void shouldReturnAListOfConferences() throws Exception {
+        List<Conference> conferences = List.of(Conference.fromDTO(conferenceDTO()));
+        given(conferenceService.findAllConferences()).willReturn(conferences);
+        mockMvc.perform(get("/conferences"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value(conferences.get(0).getName()))
+                .andExpect(jsonPath("$[0].start", matchesZonedDateTime(conferences.get(0).getStart())))
+                .andExpect(jsonPath("$[0].end", matchesZonedDateTime(conferences.get(0).getEnd())))
+                .andExpect(jsonPath("$[0].topic").value(conferences.get(0).getTopic()))
+                .andExpect(jsonPath("$[0].participantsCount").value(conferences.get(0).getParticipantsCount()));
+
+    }
+
+    @Test
+    public void shouldUpdateAConference() throws Exception {
+        Conference conference = Conference.fromDTO(conferenceDTO());
+        given(conferenceService.updateConference(any(), any())).willReturn(conference);
+        mockMvc.perform(put("/conferences/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(conferenceDTO())))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(conference.getName()))
+                .andExpect(jsonPath("$.start", matchesZonedDateTime(conference.getStart())))
+                .andExpect(jsonPath("$.end", matchesZonedDateTime(conference.getEnd())))
+                .andExpect(jsonPath("$.topic").value(conference.getTopic()))
+                .andExpect(jsonPath("$.participantsCount").value(conference.getParticipantsCount()));
+    }
 
 }
